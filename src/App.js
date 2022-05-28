8bfe1a08f4a0cd61e790d3b712a9a3f5ecd2cfaf
milad karimi <izi.bizines@gmail.com>
2022-05-28 10:09:28 +0430

    2. Routing In Our Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage/homepage'
+import HomePage from './pages/homepage/homepage';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>
+        HatsPage
+      </h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
