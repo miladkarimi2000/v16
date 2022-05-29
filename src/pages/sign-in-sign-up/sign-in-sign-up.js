e0d0862df01a462a96ee4c13e141d0204d7638c5
milad karimi <izi.bizines@gmail.com>
2022-05-29 12:20:11 +0430

    7. Sign In Component

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './sign-in-sign-up.scss';
+
+import SignIn from '../../components/sign-in/sign-in';
+
+const SignInSignUp = () => (
+    <div className="sign-in"><SignIn /></div>
+);
+
+export default SignInSignUp;
