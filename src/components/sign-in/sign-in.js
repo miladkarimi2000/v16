13a7ec4f3ecbfb0cc62e393cf1ac38b673ccb86b
milad karimi <izi.bizines@gmail.com>
2022-05-29 20:24:02 +0430

    9. Google Sign In Authentication 3

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -3,7 +3,7 @@ import './sign-in.scss';
 
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -44,8 +44,10 @@ class SignIn extends React.Component {
                         handleChange={this.handleChange}
                         required
                     />
-                    <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>Sign in with google</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with google</CustomButton>
+                    </div>
                 </form>
             </div>
         );
