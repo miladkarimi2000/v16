6334916de4c69d738625b4fadd2ac690dec735db
milad karimi <izi.bizines@gmail.com>
2022-06-01 14:57:40 +0430

    15. Sign Up With Email and Password

--- src/pages/sign-in-sign-up/sign-in-sign-up.js
+++ src/pages/sign-in-sign-up/sign-in-sign-up.js
@@ -2,9 +2,13 @@ import React from "react";
 import './sign-in-sign-up.scss';
 
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 const SignInSignUp = () => (
-    <div className="sign-in-sign-up"><SignIn /></div>
+    <div className="sign-in-sign-up">
+        <SignIn />
+        <SignUp />
+    </div>
 );
 
 export default SignInSignUp;
