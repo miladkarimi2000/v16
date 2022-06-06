c9f8d4478768f9bce9f0332c4f8eada60a5d8146
milad karimi <izi.bizines@gmail.com>
2022-06-06 08:31:59 +0430

    15. Cart Item Reducer

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => {
                 {
                     items
                         .filter((item, index) => index < 4)
-                        .map(({ id, ...restProps }) =>
-                            <CollectionItem key={id} {...restProps} />
+                        .map(item =>
+                            <CollectionItem key={item.id} item={item} />
                         )
                 }
             </div>
