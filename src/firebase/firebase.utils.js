9bb16ab0fcfdade2b02d134b4f20b0ecb6f84612
milad karimi <izi.bizines@gmail.com>
2022-06-16 16:14:10 +0430

    9. Adding Shop Data To Redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -64,7 +64,10 @@ export const convertCollectionsSnapshotToMap = collections => {
 
     });
 
-    console.log(transformedCollections);
+    return transformedCollections.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
 }
 
 
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Route } from 'react-router-dom';
+import { connect } from "react-redux";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview";
 import CollectionPage from "../collection/collection";
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component {
 
     unsybscribeFromSnapshop = null;
 
     componentDidMount() {
+        const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
         collectionRef.onSnapshot(snapshot => {
-            convertCollectionsSnapshotToMap(snapshot);
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
         })
     }
 
@@ -27,4 +31,8 @@ class ShopPage extends React.Component {
     }
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
