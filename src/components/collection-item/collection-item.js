382f5a2421b314c25e1ba99e01878ff6b310c7c3
milad karimi <izi.bizines@gmail.com>
2022-06-16 19:48:57 +0430

    The created problem in the last video in styled-components section was resolved. (add to cart button in collection-item)

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,28 +1,33 @@
-import React from "react";
-import { connect } from "react-redux";
-import './collection-item.scss';
+import React from 'react';
+import { connect } from 'react-redux';
 
-import CustomButton from '../custom-button/custom-button';
 import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ item, addItem }) => {
-    const { id, name, price, imageUrl } = item;
-    return (
-        <div className="collection-item">
-            <div
-                className="image"
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }} />
-            <div className="collection-footer">
-                <span className="name">{name}</span>
-                <span className="price">{price}</span>
-            </div>
-            <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
-        </div>
-    );
+import {
+    CollectionItemContainer,
+    CollectionFooterContainer,
+    AddButton,
+    BackgroundImage,
+    NameContainer,
+    PriceContainer
+} from './collection-item.styles';
 
-}
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+
+    return (
+        <CollectionItemContainer>
+            <BackgroundImage className='image' imageUrl={imageUrl} />
+            <CollectionFooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </CollectionFooterContainer>
+            <AddButton onClick={() => addItem(item)} inverted>
+                Add to cart
+            </AddButton>
+        </CollectionItemContainer>
+    );
+};
 
 const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item))
