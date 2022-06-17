7fea4c55f1697d26a065ab71bce62733d416fd17
milad karimi <izi.bizines@gmail.com>
2022-06-17 08:36:38 +0430

    4. Optional How To Build HOCs

--- src/App.js
+++ src/App.js
@@ -1,68 +1,21 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import HomePage from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop';
-import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up';
-import CheckoutPage from './pages/checkout/checkout';
+import UserProfile from './components/user-profile/user-profile.component';
+import UserList from './components/user-list/user-list.component';
 
-import Header from './components/header/header';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
-import { selectCurrentUser } from './redux/user/user.selectors';
+import './App.scss';
 
-import './App.css';
-
-class App extends React.Component {
-
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          });
-        });
-      }
-      else {
-        setCurrentUser(userAuth);
-      }
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to="/" /> : <SignInSignUp />} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='App'>
+      <UserList dataSource='https://jsonplaceholder.typicode.com/users' />
+      <UserProfile
+        name='Yihua'
+        email='yihuazhang@gmail.com'
+        dataSource='https://jsonplaceholder.typicode.com/posts'
+      />
+    </div>
+  );
 }
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-});
-
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
