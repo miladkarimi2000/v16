d651659243d670ed66bc77830c6a390be8f38363
milad karimi <izi.bizines@gmail.com>
2022-06-19 15:11:56 +0430

    5. Redux Thunk Into Saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,17 @@
-import { takeEvery } from 'redux-saga/effects';
-
+import { call, put, takeEvery } from 'redux-saga/effects';
 import { ShopActionTypes } from './shop.types';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
 export function* fetchCollectionsAsync() {
-    yield console.log('I am fired');
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
 }
 
 export function* fetchCollectionsStart() {
