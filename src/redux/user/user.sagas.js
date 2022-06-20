46c198826ea728b82a861d5319470f1308aa0776
milad karimi <izi.bizines@gmail.com>
2022-06-20 15:30:10 +0430

    13. Sign Out With Sagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,6 +5,8 @@ import UserActionTypes from './user.types';
 import {
     signInSuccess,
     signInFailure,
+    signOutSuccess,
+    signOutFailure
 } from './user.actions';
 
 import {
@@ -65,10 +67,25 @@ export function* onCheckUserSession() {
     yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated)
 }
 
+
+export function* signOut() {
+    try {
+        yield auth.signOut();
+        yield put(signOutSuccess());
+    } catch (error) {
+        yield put(signOutFailure(error));
+    }
+}
+
+export function* onSignOutStart() {
+    yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut)
+}
+
 export function* userSagas() {
     yield all([
         call(onGoogleSignInStart),
         call(onEmailSignInStart),
-        call(onCheckUserSession)
+        call(onCheckUserSession),
+        call(onSignOutStart),
     ]);
 }
